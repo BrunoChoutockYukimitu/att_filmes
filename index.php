7b3e3d7e908816ef6da7526eb02d9fc29babdc88
Bruno Choutock Yukimitu <brunochoutockyk@gmail.com>
2025-06-02 19:51:02 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+include "cabecalho.php";
+include "banner.php";
+?>
+
+<h2>Melhores avaliações</h2>
+<div class="lista-filmes">
+    <div class="card-filme">
+        <img src="imagens/bee movie.jpg">
+        <h3>Bee Movie</h3>
+        <span>⭐ 10/10</span>
+    </div>
+    <div class="card-filme">
+        <img src="imagens/fnaf filme.jpg">
+        <h3>FNAF Movie</h3>
+        <span>⭐ 10/10</span>
+    </div>
+    <div class="card-filme">
+        <img src="imagens/todo mudo em panico.jfif">
+        <h3>Todo Mundo em Panico</h3>
+        <span>⭐ 10/10</span>
+    </div>
+</div>
+<?php include "rodape.php"; ?>
---
+++ listadefilmes.php
@@ -0,0 +1,52 @@
+<?php
+include "cabecalho.php";
+?>
+<h2>Todos os filmes</h2>
+<div class="lista-filmes">
+    <div class="card-filme">
+        <img src="imagens/bee movie.jpg">
+        <h3>Bee Movie</h3>
+        <span>⭐ 10/10</span>
+    </div>
+    <div class="card-filme">
+        <img src="imagens/fnaf filme.jpg">
+        <h3>FNAF Movie</h3>
+        <span>⭐ 10/10</span>
+    </div>
+    <div class="card-filme">
+        <img src="imagens/todo mudo em panico.jfif">
+        <h3>Todo Mundo em Panico</h3>
+        <span>⭐ 10/10</span>
+    </div>
+    <div class="card-filme">
+        <img src="imagens/bee movie.jpg">
+        <h3>Bee Movie</h3>
+        <span>⭐ 10/10</span>
+    </div>
+    <div class="card-filme">
+        <img src="imagens/fnaf filme.jpg">
+        <h3>FNAF Movie</h3>
+        <span>⭐ 10/10</span>
+    </div>
+    <div class="card-filme">
+        <img src="imagens/todo mudo em panico.jfif">
+        <h3>Todo Mundo em Panico</h3>
+        <span>⭐ 10/10</span>
+    </div>
+    <div class="card-filme">
+        <img src="imagens/bee movie.jpg">
+        <h3>Bee Movie</h3>
+        <span>⭐ 10/10</span>
+    </div>
+    <div class="card-filme">
+        <img src="imagens/fnaf filme.jpg">
+        <h3>FNAF Movie</h3>
+        <span>⭐ 10/10</span>
+    </div>
+    <div class="card-filme">
+        <img src="imagens/todo mudo em panico.jfif">
+        <h3>Todo Mundo em Panico</h3>
+        <span>⭐ 10/10</span>
+    </div>
+</div>
+<?php include "rodape.php"; ?>
