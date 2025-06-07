64d32349864cfc981193456f4b907b2795a988d3
Bruno Choutock Yukimitu <brunochoutockyk@gmail.com>
2025-06-06 22:18:51 -0300

    fregvcs

--- index.php
+++ index.php
@@ -2,23 +2,24 @@
 include "cabecalho.php";
 include "banner.php";
 ?>
-
-<h2>Melhores avaliações</h2>
-<div class="lista-filmes">
-    <div class="card-filme">
+<div class="container">
+<h2 class="display-5">Melhores avaliações</h2>
+<div class="row">
+    <div class="col-3">
         <img src="imagens/bee movie.jpg">
         <h3>Bee Movie</h3>
         <span>⭐ 10/10</span>
     </div>
-    <div class="card-filme">
+    <div class="col-3">
         <img src="imagens/fnaf filme.jpg">
         <h3>FNAF Movie</h3>
         <span>⭐ 10/10</span>
     </div>
-    <div class="card-filme">
+    <div class="col">
         <img src="imagens/todo mudo em panico.jfif">
         <h3>Todo Mundo em Panico</h3>
         <span>⭐ 10/10</span>
     </div>
+</div>
 </div>
 <?php include "rodape.php"; ?>
--- rodape.php
+++ rodape.php
@@ -1,7 +1,10 @@
 <footer class="rodape">
-        <hr>
-        <img src="imagens/logo.png" class="logo">
-        <small><?php echo date("Y"); ?></small>
-    </footer>
+    <hr>
+    <img src="imagens/logo.png" class="logo">
+    <small><?php echo date("Y"); ?></small>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+ integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
+
 </html>
