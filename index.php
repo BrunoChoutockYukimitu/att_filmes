c7322d31fd54526da4c2956789b5875e5b8fdae3
Bruno Choutock Yukimitu <brunochoutockyk@gmail.com>
2025-06-09 22:17:20 -0300

    ~jm

--- index.php
+++ index.php
@@ -4,43 +4,55 @@ include "banner.php";
 ?>
 <div class="container">
     <h2 class="display-5">Melhores avaliações</h2>
-    <div class="row mb-5">
-        <div class="col-3">
-            <img src="filmes/bee movie.jpg" class="img-fluid">
-            <h3>Bee Movie</h3>
-            <span>⭐ 10/10</span>
+    <div class="row">
+        <div class="col-3 mb-4">
+            <div class="card" style="width: 18rem;">
+                <a href="umfilme.php"><img src="filmes/bee movie.jpg" class="card-img-top"></a>
+                <h3>Bee Movie</h3>
+                <span>⭐ 10/10</span>
+                <a href="umfilme.php" class="btn btn-primary">Ver detalhes</a>
+            </div>
         </div>
-        <div class="col-3">
-            <img src="filmes/fnaf filme.jpg" class="img-fluid">
-            <h3>FNAF Movie</h3>
-            <span>⭐ 10/10</span>
+        <div class="col-3 mb-4">
+            <div class="card" style="width: 18rem;">
+                <a href="#"><img src="filmes/fnaf filme.jpg" class="card-img-top"></a>
+                <h3>FNAF Movie</h3>
+                <span>⭐ 10/10</span>
+                <a href="umfilme.php" class="btn btn-primary">Ver detalhes</a>
+            </div>
         </div>
-        <div class="col-3">
-            <img src="filmes/todo mudo em panico.jfif" class="img-fluid">
-            <h3>Todo Mundo em Panico</h3>
-            <span>⭐ 10/10</span>
+        <div class="col-3 mb-4">
+            <div class="card" style="width: 18rem;">
+                <a href="#"><img src="https://th.bing.com/th/id/OIP.Pqa0Lg7F3eJdNytAtLIEUAAAAA?rs=1&pid=ImgDetMain" class="card-img-top"></a>
+                <h3>Todo Mundo em Pânico</h3>
+                <span>⭐ 10/10</span>
+                <a href="umfilme.php" class="btn btn-primary">Ver detalhes</a>
+            </div>
         </div>
-        <div class="col-3">
-            <img src="filmes/todo mudo em panico.jfif" class="img-fluid">
-            <h3>Todo Mundo em Panico</h3>
-            <span>⭐ 10/10</span>
+        <div class="col-3 mb-4">
+            <div class="card" style="width: 18rem;">
+                <img src="https://2.bp.blogspot.com/-u74DizTOw40/WI3gGxmt89I/AAAAAAAARqY/84YOgfYV8u0O0tSN4ay0SQglT1Lly2-igCLcB/s1600/Poster_Ateoultimohomem-1.jpg" class="card-img-top">
+                <h3>Até o Último Homem</h3>
+                <span>⭐ 10/10</span>
+                <a href="umfilme.php" class="btn btn-primary">Ver detalhes</a>
+            </div>
         </div>
-    </div>
+
 
     <div class="row mt-5">
         <div class="col-6">
-            <img src="https://film-book.com/wp-content/uploads/2009/06/the-last-airbender-teaser-trailer-header.jpg" class="img-fluid">
+            <img src="https://film-book.com/wp-content/uploads/2009/06/the-last-airbender-teaser-detalhes-header.jpg" class="img-fluid">
         </div>
         <div class="col-6 align-content-center">
             <p>Avatar: The Last Airbender, também conhecida como Avatar: The Legend of Aang (bra: Avatar: O Último Mestre do Ar; prt: Avatar: O Último Airbender), é uma série de televisão estadunidense de aventura e fantasia. É uma adaptação live-action da série de televisão de animação de mesmo nome (2005–2008).</p>
-            <a href="https://youtube.com" class="btn btn-success btn-lg">ASSISTA AO TRAILER</a>
+            <a href="https://youtube.com" class="btn btn-success btn-lg">ASSISTA AO detalhes</a>
         </div>
     </div>
 
     <div class="row">
     <div class="col-6 align-content-center">
             <p>The Dark Knight Rises is the finale of Christopher Nolan's Batman trilogy. It is set eight years after the events of The Dark Knight and follows a retired Wayne as Batman to save Gotham City from nuclear destruction by the terrorist Bane2. The film is celebrated for its grand scale, thrilling action, and standout performances by Christian Bale and Tom Hardy1.</p>
-            <a href="https://youtube.com" class="btn btn-success btn-lg">ASSISTA AO TRAILER</a>
+            <a href="https://youtube.com" class="btn btn-success btn-lg">ASSISTA AO detalhes</a>
         </div>
         <div class="col-6">
             <img src="https://th.bing.com/th/id/R.1575408649924b12d9fd8e4aca122552?rik=6O%2fILcaN%2brc7Wg&riu=http%3a%2f%2fwww.impawards.com%2f2012%2fposters%2fdark_knight_rises_ver13_xxlg.jpg&ehk=ow6IA89C%2f7JG3tnSJYsO6cctspsZ9QOuLkK7VnGBh0I%3d&risl=&pid=ImgRaw&r=0" class="img-fluid">
--- listadefilmes.php
+++ listadefilmes.php
@@ -9,7 +9,7 @@ include "cabecalho.php";
                 <a href="umfilme.php"><img src="filmes/bee movie.jpg" class="card-img-top"></a>
                 <h3>Bee Movie</h3>
                 <span>⭐ 10/10</span>
-                <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                <a href="umfilme.php" class="btn btn-primary">Ver trailer</a>
             </div>
         </div>
         <div class="col-3 mb-4">
@@ -17,7 +17,7 @@ include "cabecalho.php";
                 <a href="#"><img src="filmes/fnaf filme.jpg" class="card-img-top"></a>
                 <h3>FNAF Movie</h3>
                 <span>⭐ 10/10</span>
-                <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                <a href="umfilme.php" class="btn btn-primary">Ver trailer</a>
             </div>
         </div>
         <div class="col-3 mb-4">
@@ -25,7 +25,7 @@ include "cabecalho.php";
                 <a href="#"><img src="https://th.bing.com/th/id/OIP.Pqa0Lg7F3eJdNytAtLIEUAAAAA?rs=1&pid=ImgDetMain" class="card-img-top"></a>
                 <h3>Todo Mundo em Pânico</h3>
                 <span>⭐ 10/10</span>
-                <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                <a href="umfilme.php" class="btn btn-primary">Ver trailer</a>
             </div>
         </div>
         <div class="col-3 mb-4">
@@ -33,7 +33,7 @@ include "cabecalho.php";
                 <img src="https://2.bp.blogspot.com/-u74DizTOw40/WI3gGxmt89I/AAAAAAAARqY/84YOgfYV8u0O0tSN4ay0SQglT1Lly2-igCLcB/s1600/Poster_Ateoultimohomem-1.jpg" class="card-img-top">
                 <h3>Até o Último Homem</h3>
                 <span>⭐ 10/10</span>
-                <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                <a href="umfilme.php" class="btn btn-primary">Ver trailer</a>
             </div>
         </div>
 
@@ -42,7 +42,7 @@ include "cabecalho.php";
                 <img src="https://th.bing.com/th/id/OIP.EykxvAOtSSYHxUkoPe4mjwHaKb?rs=1&pid=ImgDetMain" class="card-img-top">
                 <h3>Batman Begins</h3>
                 <span>⭐ 10/10</span>
-                <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                <a href="umfilme.php" class="btn btn-primary">Ver trailer</a>
             </div>
         </div>
 
@@ -51,7 +51,7 @@ include "cabecalho.php";
                 <img src="https://th.bing.com/th/id/OIP.vRwbjz9GfUbuWPIVVLp8dwHaLH?rs=1&pid=ImgDetMain" class="card-img-top">
                 <h3>Piratas do Caribe</h3>
                 <span>⭐ 10/10</span>
-                <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                <a href="umfilme.php" class="btn btn-primary">Ver trailer</a>
             </div>
         </div>
         <div class="col-3 mb-4">
@@ -59,7 +59,7 @@ include "cabecalho.php";
                 <img src="https://th.bing.com/th/id/R.8475fc1b3562be3214ec597c8ec55e74?rik=A3KWngEQOGvSxQ&riu=http%3a%2f%2fbr.web.img2.acsta.net%2fmedias%2fnmedia%2f18%2f87%2f89%2f63%2f19962658.jpg&ehk=efZxqB7Ec8RuC4rPvcK5ZHt07VyJctjyJyf58hGkWpY%3d&risl=&pid=ImgRaw&r=0" class="card-img-top">
                 <h3>Ta chovendo hamburguer</h3>
                 <span>⭐ 10/10</span>
-                <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                <a href="umfilme.php" class="btn btn-primary">Ver trailer</a>
             </div>
         </div>
 
@@ -68,7 +68,7 @@ include "cabecalho.php";
                 <img src="https://th.bing.com/th/id/OIP.qn9h92zhCgbEwNB-hGnUygHaHX?rs=1&pid=ImgDetMain" class="card-img-top">
                 <h3>Ela dança eu danço</h3>
                 <span>⭐ 10/10</span>
-                <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                <a href="umfilme.php" class="btn btn-primary">Ver trailer</a>
             </div>
         </div>
     </div>
