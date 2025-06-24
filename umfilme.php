3937bcc1830dddf9dc2bc653712ad68c8212d238
Bruno Choutock Yukimitu <brunochoutockyk@gmail.com>
2025-06-23 21:11:35 -0300

    fim

--- umfilme.php
+++ umfilme.php
@@ -8,34 +8,35 @@ $id = $_GET['id'];
 //laço com as informações do filme
 //mostrar as informações na tela
 //fechar o bd
- ?>
+?>
 <?php
 include "conexao.php";
-       
- 
- 
-        $sql = "select * from filmes where id = " . $id;
-        $resultado = mysqli_query($conexao,$sql);
-       
-        //echo "<pre>";
-        //print_r($resultado);
-        //exit();
-        while($linha = mysqli_fetch_assoc($resultado) ){
-          ?>
-          <div class="container">
-            <h2><?=$linha['titulo'];?></h2>
 
-            <div class="text-center">
-  <img src="<?=$linha['foto'];?>" class="rounded" alt="filme_imagem">
-</div>
-        
-            <p>⭐ <?=$linha['avaliacao'];?>/10</p>
-            <p><strong>Categoria: </strong> <?=$linha['categoria'];?> </p>
-            <p><strong>História:</strong> <?=$linha['historia'];?></p>
-        </div>
-        <?php
-        }
-        mysqli_close($conexao);
-        ?>
+
+
+$sql = "select * from filmes where id = " . $id;
+$resultado = mysqli_query($conexao, $sql);
+
+//echo "<pre>";
+//print_r($resultado);
+//exit();
+while ($linha = mysqli_fetch_assoc($resultado)) {
+?>
+  <div class="container">
+    <div class="row mx-5 mt-5">
+      <div class="col">
+      <img src="<?= $linha['foto']; ?>" class="img-fluid" alt="filme_imagem">
+      </div>
+      <div class="col">
+        <h2><?= $linha['titulo']; ?></h2>
+      <p>⭐ <?= $linha['avaliacao']; ?>/10</p>
+      <p><strong>Categoria: </strong> <?= $linha['categoria']; ?> </p>
+      <p><strong>História:</strong> <?= $linha['historia']; ?></p>
+      </div>
+    </div>
+  </div>
+<?php
+}
+mysqli_close($conexao);
+?>
 <?php include "rodape.php"; ?>
- 
